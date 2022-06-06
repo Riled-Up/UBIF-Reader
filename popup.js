0ca5fb6cad70a9c3f2921a8641b1e12842dc4e57
Riley Guy <rileygriffinguy@gmail.com>
2022-06-06 01:36:52 -0400

    Download button is available based on URL
    with beautiful colors too

--- popup.js
+++ popup.js
@@ -1,17 +1,21 @@
-function makeBtn() {
+makeBtn();
+
+async function makeBtn() {
 
 // Go ahead and create our button
 var btn = document.createElement("button");
 btn.type = "button";
 
 // Get the current tab from background.js
-var bgPage = chrome.extension.getBackgroundPage();
-var currTab =  bgPage.getCurrentTab();
+let queryOptions = { active: true, lastFocusedWindow: true };
+let [currTab] = await chrome.tabs.query(queryOptions);
 
 // Change the text of our button according to the URL
 if (currTab.url.slice(0, 24) == "https://portal.ubif.net/") {
+	btn.style.background = "LightGreen";
 	btn.innerHTML = "Download Available";
 } else {
+	btn.style.background = "LightCoral";
 	btn.innerHTML = "Download Unavailable";
 }
 
